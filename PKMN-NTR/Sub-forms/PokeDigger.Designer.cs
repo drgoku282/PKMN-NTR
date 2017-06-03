4edeb29fc512557ab7f75a4d7f935a7b501930f7
drgoku282 <drgokulyrics@gmail.com>
2017-06-03 18:51:17 -0500

    Fix odd behavior of PokéDigger window (fix #88)

--- PKMN-NTR/Sub-forms/PokeDigger.Designer.cs
+++ PKMN-NTR/Sub-forms/PokeDigger.Designer.cs
@@ -355,6 +355,7 @@
             this.Controls.Add(this.ResultsGrid);
             this.Controls.Add(this.InfoLabel);
             this.Controls.Add(this.DoItBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "PokeDigger";
