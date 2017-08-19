4bb83017aa70bd2bbdfbed09fe2be9141a75ac40
drgoku282 <drgokulyrics@gmail.com>
2017-08-19 14:20:46 -0500

    Update PKHeX Core to 20170807

--- PKMN-NTR/Properties/AssemblyInfo.cs
+++ PKMN-NTR/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("cd7cfe42-3900-4148-aa42-6a04af45b783")]
 
 [assembly: AssemblyVersion("3.5.*")]
-[assembly: AssemblyInformationalVersion("3.5-beta1")]
+[assembly: AssemblyInformationalVersion("3.5-beta2")]
